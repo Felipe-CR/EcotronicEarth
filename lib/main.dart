4c4d3597bd3cfec8ad123935d7a5e9c66cb7e437
Felipe-CR <felicampos3942@gmail.com>
2022-11-06 22:58:13 -0500

    Cambio de Nombre

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Ecotronic Earth",
+      title: "Handle",
       theme: theme(),
       initialRoute: SignInScreen.routeName,
       routes: routes,
